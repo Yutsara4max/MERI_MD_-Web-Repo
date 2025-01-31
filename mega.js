78ab81c00dabc3b3beb9333b77925fc5eb150207
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-01-31 12:08:13 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'dinuraillwayfourth@gmail.com',
-    password: 'DINUdinu12342009@',
+    email: 'Onlinecashworks@gmail.com',
+    password: 'Hettigewattha#456123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
